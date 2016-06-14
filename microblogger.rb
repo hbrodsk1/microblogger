a998ce2a9811d7927dbf513542aaf8b52134944d
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-06-14 13:21:30 -0400

    Send tweets from command line interface

--- microblogger.rb
+++ microblogger.rb
@@ -16,7 +16,24 @@ class MicroBlogger
 			puts "Your tweet contains #{message.length} characters"
 		end
 	end
+
+	def run
+		puts "Welcome to the JSL Twitter Client!"
+		command = ""
+		while command != "q"
+			printf "enter command: "
+			input = gets.chomp
+			parts = input.split(" ")
+			command = parts[0]
+			case command
+			when "q" then puts "Goodbye!"
+			when "t" then tweet(parts[1..-1].join(" "))
+			else
+				puts "Sorry, I don't know how to #{command}"
+			end
+		end
+	end
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("a" * 140)
+blogger.run
